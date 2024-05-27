6cbd811f88a9c154b4ee32c196b30bd82960e852
BeomjunKim123 <gkgk9753@gmail.com>
2024-05-27 11:02:45 +0900

    AWS 리소스용 테라폼 변수 정의

---
+++ variables.tf
@@ -0,0 +1,28 @@
+#variable -> 테라폼 입력 변수를 정의한다. 
+variable "region" {
+  description = "The region where AWS resources will be created"
+  default     = "ap-northeast-2"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  default     = "my-eks-cluster"
+}
+
